c3bb3165a205e4536d1e14f3f20e7c9e4f4b9717
hao3842850 <144036377+hao3842850@users.noreply.github.com>
2025-12-12 03:56:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 # === MongoDB 名冊系統 ===
 from pymongo import MongoClient
 
-MONGO_URL = "mongodb+srv://hao:wenhao0222@cluster0.utvdkw9.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true&ssl=true"
-
-mongo_client = MongoClient(
-    MONGO_URL,
-    serverSelectionTimeoutMS=20000
-)
-
-mongo_db = mongo_client["line_bot"]
+MONGO_URL = "mongodb+srv://hao:wenhao0222@cluster0.utvdkw9.mongodb.net/linebot?retryWrites=true&w=majority&appName=Cluster0"
+mongo_client = MongoClient(MONGO_URL)
+mongo_db = mongo_client["linebot"]
 roster_collection = mongo_db["roster"]
-
 
 from fastapi import FastAPI, Request, Header
 from linebot import LineBotApi, WebhookHandler
